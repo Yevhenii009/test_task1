278302c2e11f265bd44a27f4d8dd5074b0dc161c
Yevhenii009 <stifgun@gmail.com>
2023-05-09 16:57:14 +0300

    тестоведз

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { App } from './components/App';
+import { GlobalStyle } from 'constants/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'constants';
+import 'react-toastify/dist/ReactToastify.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/test_task1">
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+          <GlobalStyle />
+          <ToastContainer />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
